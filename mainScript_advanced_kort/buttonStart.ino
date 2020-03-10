30706fe70d1a04a796141f0b276f03a27f4e0d0e
jbolvig <61268133+jbolvig@users.noreply.github.com>
2020-03-10 13:23:15 +0100

    Delete buttonStart.ino

--- mainScript_advanced_kort/buttonStart.ino
+++
@@ -1,8 +0,0 @@
-// Setup function for push button with LEDs
-
-void buttonStart()  {
-  // Defining pins as input and output
-  pinMode(ledPinA, OUTPUT);
-  pinMode(ledPinB, OUTPUT);
-  pinMode(buttonPin, INPUT);
-}
